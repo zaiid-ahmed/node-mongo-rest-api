e6890cb321d3fe9d74649e6e2bf8e883e6e3f3c7
Zaid Ahmed <zk4467450@gmail.com>
2026-04-02 06:24:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/')
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.log('Error', err));
+
+const itemSchema = new mongoose.Schema({
+    name: String
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+
+app.get('/items', async (req, res) => {
+    const items = await Item.find();
+    res.json(items);
+});
+
+
+app.get('/items/:id', async (req, res) => {
+    const item = await Item.findById(req.params.id);
+    res.json(item);
+});
+
+
+app.post('/items', async (req, res) => {
+    const newItem = new Item({ name: req.body.name});
+    await newItem.save();
+    res.json(newItem);
+});
+
+
+app.put('/items/:id', async (req, res) => {
+    const updated = await Item.findByIdAndUpdate(
+        req.params.id,
+        {name: req.body.name},
+        {new: true} 
+    );
+    res.json(updated);
+});
+
+
+app.delete('/items/:id', async (req, res) => {
+    await Item.findByIdAndDelete(req.params.id);
+    res.json({message: 'Deleted'});
+});
+
+
+app.listen(3000, () => console.log('Server running on port 3000'));
